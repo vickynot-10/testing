932c0d2bfd7133fad0ffb7f7ac2260404a5c7349
Vignesh <154013328+vickynot-10@users.noreply.github.com>
2026-03-22 16:47:52 +0530

    Refactor fetchUser to synchronous db querykk
    Refactor fetchUser function to remove async behavior and cache logic.

--- test.js
+++ test.js
@@ -1,26 +1,4 @@
-async function fetchUser(id) {
-  const cached = await cache.get(id);
-  if (cached) return cached;
-  const user = await db.query(id);
-  await cache.set(id, user, 300);
-  logger.info("cache miss", { id });
+function fetchUser(id) {
+  const user = db.query(id);
   return user;
-}
-
-function calculateTotal(items) {
-  return items.reduce((sum, item) => sum + item.price * item.quantity, 0);
-}
-
-async function sendEmail(to, subject, message) {
-  await mailer.send({ to, subject });
-  logger.info("email sent", { to });
-}
-
-async function deleteUser(id) {
-  const user = await fetchUser(id);
-  if (!user) throw new Error("User not found");
-  await db.query("DELETE FROM users WHERE id = ?", id);
-  await cache.delete(id);
-  logger.info("user deleted", { id });
-  return true;
 }
